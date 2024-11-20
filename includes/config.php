2ec1b2e3d57f85e859d8160b8322f181822e5fb8
J817B477 <Entropythicus@gmail.com>
2024-11-20 12:52:25 -0500

    establishes db access and encryption credentials

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,11 @@
 <?php
+session_start();
+// generates DB access credentials
 const DBNAME = "student_passwords";
 const DBHOST = "localhost";
 const DBUSER = "passwords_user";
-const DBPASS = "f(D2Whiue9d8yD";
 
-
-// needs AES variables
+// generates encryption credentials
+const KEY_STR = "JS04*KR_NTO{1112";
+const INNIT_VECTOR = "LKJSE29448NDJSSK";
+const AESCONFIG = 'aes-256-cbc';
