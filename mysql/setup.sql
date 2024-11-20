1f4ae0cdd5fe4afff8f33d042e8b133a6cb94b20
J817B477 <Entropythicus@gmail.com>
2024-11-20 12:50:04 -0500

    key relationship and aes credential updates adds id attributes to each table and adjusts schema respectively; changes aes credentials to a functional method

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -1,3 +1,4 @@
+\W
 DROP DATABASE IF EXISTS student_passwords;
 
 CREATE DATABASE student_passwords DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
@@ -6,40 +7,68 @@ USE student_passwords;
 
 DROP USER IF EXISTS 'passwords_user'@'localhost';
 
-CREATE USER 'passwords_user'@'localhost' IDENTIFIED BY 'f(D2Whiue9d8yD';
+CREATE USER 'passwords_user'@'localhost';
 GRANT ALL ON student_passwords.* TO 'passwords_user'@'localhost';
 
 
--- generates session arguments for AES_Encrypt()
-SET block_encryption_mode = 'aes-256-cbc';
-SET @key_str = UNHEX(SHA2('encrypt this', 256));
-SET @init_vector = RANDOM_BYTES(16);
+-- generates arguments for AES_Encrypt()
+SET @block_encryption_mode = 'aes-256-cbc';
+-- SET @key = 'encrypt this';
+-- SET @vec = 'and this';
+SET @key_str = "JS04*KR_NTO{1112";
+SET @init_vector = "LKJSE29448NDJSSK";
+
+-- creates encryption credentials table
+DROP TABLE IF EXISTS credentials;
+CREATE TABLE credentials (
+  key_id SMALLINT AUTO_INCREMENT PRIMARY KEY,
+  enc_mode VARCHAR(257) NOT NULL,
+  key_st VARCHAR(257) NOT NULL,
+  vec_st VARBINARY(257) NOT NULL
+);
+
+-- populates credentials table with AES arguments
+INSERT INTO credentials
+  (enc_mode,key_st,
+  vec_st)
+VALUES
+  (@block_encryption_mode,@key, @init_vector);
+
+
 
 -- creates 'student_passwords' relations
 CREATE TABLE IF NOT EXISTS user (
+  user_id SMALLINT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
-  email_address VARCHAR(65) NOT NULL,
-  PRIMARY KEY (email_address)
-);
+  email_address VARCHAR(65) NOT NULL
+)ENGINE=INNODB;
 
 CREATE TABLE IF NOT EXISTS website (
+  site_id SMALLINT AUTO_INCREMENT PRIMARY KEY,
   site_name VARCHAR(100) NOT NULL,
-  url VARCHAR(256) NOT NULL,
-  PRIMARY KEY (url)
-);
+  url VARCHAR(256) NOT NULL
+)ENGINE=INNODB;
 
 CREATE TABLE IF NOT EXISTS registry(
-  email_address VARCHAR(65) NOT NULL,
-  url VARCHAR(256) NOT NULL,
+  registry_id SMALLINT AUTO_INCREMENT PRIMARY KEY,
+  user_id SMALLINT NOT NULL,
+  site_id SMALLINT NOT NULL,
   encrypted_password VARBINARY(256) NOT NULL,
   user_name VARCHAR(65) NOT NULL,
-  timestamp TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+  timestamp TIMESTAMP(3)
+    DEFAULT CURRENT_TIMESTAMP(3)
+    ON UPDATE CURRENT_TIMESTAMP(3),
   comment VARCHAR(1000),
-  PRIMARY KEY (email_address,url),
-  FOREIGN KEY (email_address) REFERENCES user (email_address),
-  FOREIGN KEY (url) REFERENCES website (url)
-);
+  FOREIGN KEY (user_id)
+    REFERENCES user (user_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (site_id)
+    REFERENCES website (site_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+)ENGINE=INNODB;
 
 -- initial population of 'student_passwords' relations
 INSERT INTO user
@@ -65,15 +94,50 @@ INSERT INTO website
 
 -- Inserts 1st 10 password entries
  INSERT INTO registry
-    (email_address, url, encrypted_password, user_name)
+    (user_id, site_id, encrypted_password, user_name)
     VALUES
-    ('jb@gmail.com', 'https://github.com', AES_ENCRYPT('sticky putty', @key_str, @init_vector), 'J817B477'),
-    ('jb@gmail.com', 'https://www.amazon.com', AES_ENCRYPT('kerosene heater', @key_str, @init_vector), 'John Bennett'),
-    ('ian.malcolm@outlook.com', 'https://www.nypremiumoil.com', AES_ENCRYPT('chaos theory', @key_str, @init_vector),'Malcolm.I'),
-    ('johnbennett@hartford.edu', 'https://www.ctpremiumoil.com', AES_ENCRYPT('sad dinosaur', @key_str, @init_vector), 'John Bennett'),
-    ('johnbennett@hartford.edu', 'https://accounts.spotify.com/', AES_ENCRYPT('rebel music', @key_str, @init_vector), 'J477'),
-    ('g.sanderson@3blue1brown.com', 'https://github.com', AES_ENCRYPT('math in motion', @key_str, @init_vector), '3b1b'),
-    ('g.sanderson@3blue1brown.com', 'https://accounts.spotify.com/', AES_ENCRYPT('math is music', @key_str, @init_vector), '3b1b'),
-    ('g.sanderson@3blue1brown.com', 'https://www.linkedin.com/', AES_ENCRYPT('free conferences', @key_str, @init_vector), 'Grant Sanderson'),
-    ('jj@joshjohnsoncomedy.com', 'https://www.nytimes.com/', AES_ENCRYPT('not funny material', @key_str, @init_vector), 'J.Johnson'),
-    ('jj@joshjohnsoncomedy.com', 'https://www.linkedin.com/', AES_ENCRYPT('comedy business', @key_str, @init_vector), 'Josh Johnson Comedy');
+    (1, 2, AES_ENCRYPT('sticky putty', @key_str, @init_vector), 'J817B477'),
+    (1, 1, AES_ENCRYPT('kerosene heater', @key_str, @init_vector), 'John Bennett'),
+    (2, 4, AES_ENCRYPT('chaos theory', @key_str, @init_vector),'Malcolm.I'),
+    (3, 3, AES_ENCRYPT('sad dinosaur', @key_str, @init_vector), 'John Bennett'),
+    (3, 5, AES_ENCRYPT('rebel music', @key_str, @init_vector), 'J477'),
+    (4, 2, AES_ENCRYPT('math in motion', @key_str, @init_vector), '3b1b'),
+    (4, 5, AES_ENCRYPT('math is music', @key_str, @init_vector), '3b1b'),
+    (4, 6, AES_ENCRYPT('free conferences', @key_str, @init_vector), 'Grant Sanderson'),
+    (5, 7, AES_ENCRYPT('not funny material', @key_str, @init_vector), 'J.Johnson'),
+    (5, 6, AES_ENCRYPT('comedy business', @key_str, @init_vector), 'Josh Johnson Comedy');
+
+-- decryption procedures
+DELIMITER $$
+CREATE PROCEDURE IF NOT EXISTS sp_create_search_table()
+BEGIN
+
+  CREATE TABLE IF NOT EXISTS decrypted_registry (
+  registry_id SMALLINT,
+  user_id SMALLINT NOT NULL,
+  site_id SMALLINT NOT NULL,
+  decrypted_password VARCHAR(256),
+  user_name VARCHAR(65) NOT NULL,
+  timestamp TIMESTAMP(3),
+  comment VARCHAR(1000)
+  ) ENGINE=INNODB;
+
+
+  INSERT INTO decrypted_registry (registry_id, user_id, site_id,decrypted_password, user_name, timestamp, comment)
+  SELECT
+    registry_id,
+    user_id,
+    site_id,
+    CAST(AES_DECRYPT(encrypted_password, @key_str, @init_vector) AS CHAR),
+    user_name,
+    timestamp,
+    comment
+  FROM registry;
+END$$
+
+CREATE PROCEDURE IF NOT EXISTS sp_delete_search_table()
+BEGIN
+DROP TABLE decrypted_registry;
+END$$
+
+DELIMITER ;
